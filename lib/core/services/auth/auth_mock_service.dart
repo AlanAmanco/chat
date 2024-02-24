f98422c8622de2f5cb5d73e1b9b719fc866e03de
alan.amanco <alan.amanco@sigep.com.br>
2024-02-24 17:26:00 -0300

    login

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -1,31 +1,58 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:chat/pages/chat_user.dart';
 
 class AuthMockService implements AuthService {
-  @override
-  ChatUser? get currentUser => throw UnimplementedError();
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
 
   @override
-  // TODO: implement userChange
-  Stream<ChatUser?> get userChange => throw UnimplementedError();
-
-  @override
-  Future<void> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  ChatUser? get currentUser {
+    return _currentUser;
   }
 
   @override
-  Future<void> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Stream<ChatUser?> get userChange {
+    return _userStream;
   }
 
   @override
-  Future<void> signup(String nome, String email, String password, File image) {
-    // TODO: implement signup
-    throw UnimplementedError();
+  Future<void> login(String email, String password) async {
+    _updateUser(_users[email]);
+  }
+
+  @override
+  Future<void> logout() async {
+    _updateUser(null);
+  }
+
+  @override
+  Future<void> signup(
+    String name,
+    String email,
+    String password,
+    File? image,
+  ) async {
+    final newUser = ChatUser(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      imageURL: image?.path ?? 'lib/assets/images/avatar.png',
+    );
+    _users.putIfAbsent(email, () => newUser);
+    _updateUser(newUser);
+  }
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
   }
 }
